30ffb5ad5a313e0cf67a9aa4ed5eb492556640e8
neelarya19 <neelarya19@gmail.com>
2022-01-08 15:28:32 +0530

    update urls.py file

--- core/urls.py
+++ core/urls.py
@@ -5,7 +5,6 @@ app_name='core'
 
 urlpatterns = [
     path('',views.PlayView.as_view(),name='play'),
-    path('results/',views.results.as_view(),name='results'),
     path('round/',views.RoundView.as_view(),name='round')
     
 ]
